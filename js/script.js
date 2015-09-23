85cc1f209a161af466dc843441e1ff6f63faa0aa
Alyssa Williams <aswilliams9@gmail.com>
2015-09-23 12:25:22 -0400

    Changed complete task logic

--- js/script.js
+++ js/script.js
@@ -2,31 +2,30 @@ var app = angular.module("todoApp", []);
 app.controller("todoCtrl", function($scope, $http) {
 
 	$scope.taskList = [];
-	$scope.completedTasks = 0;
-	$scope.error = false;
-	$scope.done = false;
 
-	$http.get("https://api.myjson.com/bins/3pxvw")
+	$http.get("https://api.myjson.com/bins/2avds")
 		.success(function(result) {
 			$scope.taskList = result.data;
 		})
 		.error(function (data, status, headers, config) {
 			$scope.error = true;
-		});
+	});
 
-		$scope.completeTask = function($index) {
-			if ($scope.taskList[$index].completed === true) {
-					$scope.completedTasks++;
-			}
-			else {
-				$scope.completedTasks--;
-			}
+	$scope.completedTasks = 0;
 
-			if ($scope.completedTasks == $scope.taskList.length) {
-				$scope.done = true;
-	 		}
-	 		else {
-	 			$scope.done = false;
-	 		}
-	 	};
+	$scope.completeTask = function(task) {
+		if (task.status == "completed") {
+			$scope.completedTasks++;
+		}
+		else if (task.status == "todo") {
+			$scope.completedTasks--;
+		}
+
+		if ($scope.completedTasks == $scope.taskList.length) {
+			$scope.allTasks = "done";
+		}
+		else {
+			$scope.allTasks = "count";
+		}
+	};	
 });
